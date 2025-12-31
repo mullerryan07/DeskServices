a66ff9c2618b3926ac8caaff3577f146ac463de8
Ryan Muller <127597793+mullerryan07@users.noreply.github.com>
2025-12-31 02:22:49 +0200

    DeskServices Project Files
    Created DeskServices Project File

---
+++ DeskServices.dpr
@@ -0,0 +1,14 @@
+program DeskServices;
+
+uses
+  Vcl.Forms,
+  Init_u in 'Init_u.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
